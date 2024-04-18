bb0a75cf9568d43fbf305cab0948df621bccc21f
Joao Labussiere <labussework@gmail.com>
2024-04-18 19:38:31 -0300

    Finalização!

--- README.md
+++ README.md
@@ -3,15 +3,8 @@
 O **Alura Midi** é um instrumento musical de interface digital, onde cada botão reproduz o som de um instrumento.
 
 <div  align="center">
-<img src="images\Alura-Midi.png" alt="Alura-Midi">
+<img src="images\Alura-midi.png" alt="Alura-Midi">
 </div>
-<!-- 
-Ou:
-<div  align="center">
-
-![Alura-Midi](images\Alura-Midi.png)
-
-</div> -->
 
 Sua interface gráfica possui um conjunto de nove botões brancos e circulares dispostos em uma grade 3x3. Cada botão contém uma palavra em inglês, representando diferentes sons: 'Pom', 'Clap', 'Tim', 'Puff', 'Splash', 'Toim', 'Psh', 'Tic' e 'Tom'. A palavra 'Splash' está realçada com um halo vermelhor ao redor do botão, indicando seleção ou interação.
 
--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play()
-}
+function tocarSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+    elemento === null || elemento.localName !== 'audio' ? console.error('Elemento não encontrado, ou seletor inválido') : elemento.play(); 
+}
+        
+const listaDeBotoes = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < listaDeBotoes.length; i++) {
+
+    const tecla = listaDeBotoes[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocarSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if(evento.code === "Enter" || evento.code === "Space") {
+            tecla.classList.add('ativa');
+            }
+        }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
